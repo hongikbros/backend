a14b6cfacf9de040fcc1f29934eee3c069b185e7
Eun Seok <jeseok95@gmail.com>
2021-02-14 01:20:24 +0900

    feat: Company Entity,Repository 추가
    - icon 사진은 lob 타입으로 설정

---
+++ src/main/java/com/hongikbros/jobmanager/notice/domain/company/Company.java
@@ -0,0 +1,49 @@
+package com.hongikbros.jobmanager.notice.domain.company;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import com.hongikbros.jobmanager.common.domain.BaseEntity;
+
+@Entity
+public class Company extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
+    private Long id;
+
+    private String name;
+
+    @Lob
+    private String icon;
+
+    protected Company() {
+    }
+
+    private Company(Long id, String name, String icon) {
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public static Company of(String name, String icon) {
+        return new Company(null, name, icon);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+}
