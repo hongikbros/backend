c657b2c6b08026eeca1720e823a271d70bd3b0c3
Eun Seok <jeseok95@gmail.com>
2021-02-14 01:44:33 +0900

    feat: 문서화를 위한 Documentation 추가
    - mockMvc의 빠른 테스트 특징을 이용해서 문서화까지 진행함
    - restAssured 스타일을 이용하기 위해 제공하는 RestAssuredMockMvc 사용
    - Spring boot에서 제공하는 MockBean 어노테이션을 이용해서 spring container에서 Mockbean을 사용하도록 함
    - 문서화 추가 필드를 사용하기 위해 format attribute를 추가
    - 문서화를 이쁘게 만들어주는 getDocumentRequest(), getDocumentResponse() 추가

--- src/test/java/com/hongikbros/jobmanager/common/documentation/Documentation.java
+++ src/test/java/com/hongikbros/jobmanager/common/documentation/Documentation.java
@@ -2,21 +2,32 @@ package com.hongikbros.jobmanager.common.documentation;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.snippet.Attributes.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.hongikbros.jobmanager.common.domain.security.CustomOauth2SuccessHandler;
+import com.hongikbros.jobmanager.member.application.oauth.CustomOAuth2UserService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
-@ExtendWith({RestDocumentationExtension.class, MockitoExtension.class})
+@ExtendWith({RestDocumentationExtension.class})
 public class Documentation {
+
+    @MockBean
+    protected CustomOAuth2UserService customOAuth2UserService;
+
+    @MockBean
+    protected CustomOauth2SuccessHandler customOauth2SuccessHandler;
+
     @BeforeEach
     public void setUp(
             WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
@@ -24,6 +35,10 @@ public class Documentation {
                 documentationConfiguration(restDocumentation),
                 springSecurity()
         );
+    }
+
+    protected static Attributes.Attribute getDateFormat() {
+        return key("format").value("yyyy-MM-dd");
     }
 
     protected static OperationRequestPreprocessor getDocumentRequest() {
