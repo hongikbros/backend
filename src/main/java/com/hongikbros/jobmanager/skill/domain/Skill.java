d83f9ef6fe23ee299850ad5c6a1e961b58b49c77
Eun Seok <jeseok95@gmail.com>
2021-02-14 01:32:01 +0900

    feat: skill Entity 추가

---
+++ src/main/java/com/hongikbros/jobmanager/skill/domain/Skill.java
@@ -0,0 +1,39 @@
+package com.hongikbros.jobmanager.skill.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.hongikbros.jobmanager.common.domain.BaseEntity;
+
+@Entity
+public class Skill extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "skill_id")
+    private Long id;
+
+    private String name;
+
+    protected Skill() {
+    }
+
+    private Skill(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Skill from(String name) {
+        return new Skill(null, name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
