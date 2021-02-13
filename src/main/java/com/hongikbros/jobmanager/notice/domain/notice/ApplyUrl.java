87a23e1137ad43e0a56f9dc4329ba77125877d7f
Eun Seok <jeseok95@gmail.com>
2021-02-14 01:31:36 +0900

    feat: Notice Entity 추가
    - ApplyUrl, Duration, NoticeDescription VO 를 추가
    - Duration는 startDate, endDate 로 이루어져 있음

---
+++ src/main/java/com/hongikbros/jobmanager/notice/domain/notice/ApplyUrl.java
@@ -0,0 +1,44 @@
+package com.hongikbros.jobmanager.notice.domain.notice;
+
+import java.util.Objects;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Access(AccessType.FIELD)
+@Embeddable
+public class ApplyUrl {
+
+    private String redirectUrl;
+
+    protected ApplyUrl() {
+    }
+
+    private ApplyUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+
+    public static ApplyUrl from(String redirectUrl) {
+        return new ApplyUrl(redirectUrl);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ApplyUrl applyUrl = (ApplyUrl)o;
+        return Objects.equals(redirectUrl, applyUrl.redirectUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(redirectUrl);
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+}
---
+++ src/main/java/com/hongikbros/jobmanager/notice/domain/notice/Duration.java
@@ -0,0 +1,53 @@
+package com.hongikbros.jobmanager.notice.domain.notice;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Access(AccessType.FIELD)
+@Embeddable
+public class Duration {
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    protected Duration() {
+    }
+
+    private Duration(LocalDateTime startDate, LocalDateTime endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static Duration of(LocalDateTime startDate, LocalDateTime endDate) {
+        return new Duration(startDate, endDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Duration duration = (Duration)o;
+        return Objects.equals(startDate, duration.startDate) && Objects.equals(
+                endDate, duration.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startDate, endDate);
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+}
