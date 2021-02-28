6063a7125db5bf2d845d57819c7da0e03578a96f
Eun Seok <jeseok95@gmail.com>
2021-03-01 01:21:15 +0900

    feat: Bookmark의 연관관계 aggregate 추가
    - memberId 와 noticeId 추가

--- src/main/java/com/hongikbros/jobmanager/member/domain/CurrentMember.java
+++ src/main/java/com/hongikbros/jobmanager/member/domain/CurrentMember.java
@@ -3,6 +3,7 @@ package com.hongikbros.jobmanager.member.domain;
 import java.io.Serializable;
 
 public interface CurrentMember extends Serializable {
+    
     Long getId();
 
     boolean isLogin();
--- src/main/java/com/hongikbros/jobmanager/notice/bookmark/Bookmark.java
+++ src/main/java/com/hongikbros/jobmanager/notice/bookmark/Bookmark.java
@@ -1,12 +1,17 @@
 package com.hongikbros.jobmanager.notice.bookmark;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.hongikbros.jobmanager.common.domain.Association;
 import com.hongikbros.jobmanager.common.domain.BaseEntity;
+import com.hongikbros.jobmanager.member.domain.Member;
+import com.hongikbros.jobmanager.notice.notice.domain.Notice;
 
 @Entity
 public class Bookmark extends BaseEntity {
@@ -16,14 +21,39 @@ public class Bookmark extends BaseEntity {
     @Column(name = "bookmark_id")
     private Long id;
 
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "member_id"))
+    private Association<Member> memberId;
+
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "notice_id"))
+    private Association<Notice> noticeId;
+
     protected Bookmark() {
     }
 
-    public Bookmark(Long id) {
-        this.id = id;
+    private Bookmark(
+            Association<Member> memberId,
+            Association<Notice> noticeId) {
+        this.memberId = memberId;
+        this.noticeId = noticeId;
+    }
+
+    public static Bookmark of(
+            Association<Member> memberId,
+            Association<Notice> noticeId) {
+        return new Bookmark(memberId, noticeId);
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Association<Member> getMemberId() {
+        return memberId;
+    }
+
+    public Association<Notice> getNoticeId() {
+        return noticeId;
     }
 }
