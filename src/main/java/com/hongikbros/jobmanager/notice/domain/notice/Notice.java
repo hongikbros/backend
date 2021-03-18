ae8f6f4367ec0c5eeda274d01660c696e2ba8825
Eun Seok <jeseok95@gmail.com>
2021-03-19 03:14:51 +0900

    refactor: Notice에서 연관관계에 joinColumn을 Column 어노테이션으로 잘못 작성한 부분 수정

--- src/main/java/com/hongikbros/jobmanager/notice/domain/notice/Notice.java
+++ src/main/java/com/hongikbros/jobmanager/notice/domain/notice/Notice.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import com.hongikbros.jobmanager.common.domain.BaseEntity;
@@ -20,7 +21,7 @@ public class Notice extends BaseEntity {
     private Long id;
 
     @OneToOne
-    @Column(nullable = false, name = "company_id")
+    @JoinColumn(nullable = false, name = "company_id")
     private Company company;
 
     @Column(nullable = false)
