db24b245593fa2ef149e75ecc4fc8a81bdc216ce
Eun Seok <jeseok95@gmail.com>
2021-03-24 02:49:49 +0900

    test: test html 파일을 가져오는 FileIoUtils 추가

---
+++ src/test/java/com/hongikbros/jobmanager/common/utils/exception/NotCreateBytesFromFilePath.java
@@ -0,0 +1,7 @@
+package com.hongikbros.jobmanager.common.utils.exception;
+
+public class NotCreateBytesFromFilePath extends RuntimeException {
+    public NotCreateBytesFromFilePath(String message) {
+        super(message);
+    }
+}
